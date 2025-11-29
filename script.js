28e544ead3553763f1d12c65768bfcb2b442bd22
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-11-29 12:00:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let para=document.querySelectorAll("strong");
+let link=document.querySelector("a");
+link.addEventListener("onmouseover",highlight);
+link.addEventlistener("onmouseout",return_normal);
+
 function highlight() {
     //Write your code here
-
-
+	para.forEach((strong)=>strong.style.color="rgb(0,128,0)")
 }
 
 
 function return_normal() {
     //Write your code here
+	para.forEach((strong)=>strong.style.color="rgb(0, 0, 0)")
 
     
 }
